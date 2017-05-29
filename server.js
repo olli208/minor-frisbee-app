052bfbd07b3eaa6ec2584623a3b1a3c31e39463b
Oliver <oliveroenang@gmail.com>
2017-05-29 13:53:44 +0200

    access token from api

--- server.js
+++ server.js
@@ -10,90 +10,58 @@ var path = require('path');
 var request = require('request'); // simple API requests
 var rp = require('request-promise'); // request promise thingy
 var querystring = require('querystring');
-var cookieParser = require('cookie-parser'); //parses cookier (? from spotify)
 var dotenv = require('dotenv').config(); //secret stuff
 var diff = require('deep-diff').diff; // compare objects
 
-var client_id = process.env.CLIENT_ID
-var client_secret = process.env.CLIENT_SECRET
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
 var redirect_uri = 'http://localhost:8888/callback'; // For local testing !!
-
 
 app.set('view engine' , 'ejs')
     .set('views' , path.join(__dirname, 'views'))
     .use(express.static('static'))
-    .use(cookieParser())
     .use(session({
         secret: 'superGeheim',
         resave: false,
         saveUninitialized: true
     }));
 
-var access_token,
-    refresh_token;
+var apiResponse;
 
 io.on('connect', onConnect);
+
 app.get('/', index);
 app.get('/login', login);
 app.get('/callback', callback);
 
 function index(req , res) {
-
+  res.render('index');
 }
 
 function login(req, res){
-
+  res.redirect('https://www.leaguevine.com/oauth2/authorize/?' +
+    'client_id=' + client_id +
+    '&response_type=code' +
+    '&redirect_uri=' + redirect_uri +
+    '&scope=universal');
 }
 
 function callback(req, res) {
-    var code = req.query.code || null;
-    var state = req.query.state || null;
+  console.log(req.query.code);
+  var code = req.query.code;
 
-    if (state === null || state !== storedState) {
-        res.redirect('/#' +
-            querystring.stringify({
-                error: 'state_mismatch'
-            }));
-    } else {
+  // request('https://www.leaguevine.com/oauth2/token/?client_id=' + client_id + '&client_secret=' + client_secret + '&code='+ code + '&grant_type=authorization_code' + '&redirect_uri=' + redirect_uri)
+  //   .then(function(body) {
+  //     console.log(body)
+  //     console.log('great success');
+  //   });
 
-        var authOptions = {
-            url: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: code,
-                redirect_uri: redirect_uri,
-                grant_type: 'authorization_code'
-            },
-            headers: {
-                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-            },
-            json: true
-        };
-
-        // Get acces and refresh okens from Spotify
-        request.post(authOptions, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                access_token = body.access_token;
-                refresh_token = body.refresh_token;
-
-                var options = {
-                    url: 'https://api.spotify.com/v1/me',
-                    headers: { 'Authorization': 'Bearer ' + access_token },
-                    json: true
-                };
-
-                // get user id
-                rp(options).then(function(body){playlistOwner = body.id});
-
-                res.redirect('/playlists');
-            } else {
-                // unless there's an error...
-                res.redirect('/#' +
-                    querystring.stringify({
-                        error: 'invalid_token'
-                    }));
-            }
-        });
-    }
+  request.post('https://www.leaguevine.com/oauth2/token/?client_id=' + client_id + '&client_secret=' + client_secret + '&code='+ code + '&grant_type=authorization_code' + '&redirect_uri=' + redirect_uri, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        apiResponse = body
+        res.redirect('/');
+      }
+    })
 }
 
 // SOCKET THINGIESS HERE
