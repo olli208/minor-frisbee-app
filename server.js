56a96bf7978a088e31626c6f1cb3283e57bd50f6
Oliver <oliveroenang@gmail.com>
2017-05-30 23:52:27 +0200

    team detail page added

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 var path = require('path');
-var request = require('request'); // simple API requests
-var rp = require('request-promise'); // request promise thingy
+var request = require('request');
+var rp = require('request-promise'); // request promise package
 var querystring = require('querystring');
 require('dotenv').config(); //secret stuff
 
@@ -25,8 +25,8 @@ app.set('view engine' , 'ejs')
         saveUninitialized: true
     }));
 
-var apiResponse;
-var acccessToken;
+var apiResponse,
+    acccessToken;
 
 io.on('connect', onConnect);
 
@@ -34,6 +34,7 @@ app.get('/', index);
 app.get('/login', login);
 app.get('/callback', callback);
 app.get('/teams', getTeams);
+app.get('/teams/:id', getTeamDetail);
 app.get('/games', getGames);
 
 function index (req , res) {
@@ -45,52 +46,77 @@ function index (req , res) {
 }
 
 function login (req, res) {
-  res.redirect('https://www.leaguevine.com/oauth2/authorize/?' +
-    'client_id=' + client_id +
-    '&response_type=code' +
-    '&redirect_uri=' + redirect_uri +
-    '&scope=universal');
+  res.redirect('https://www.leaguevine.com/oauth2/authorize/?' + 'client_id=' + client_id + '&response_type=code' + '&redirect_uri=' + redirect_uri + '&scope=universal');
 }
 
 function callback (req, res) {
   var code = req.query.code;
 
-  rp('https://www.leaguevine.com/oauth2/token/' +
-    '?client_id=' + client_id +
-    '&client_secret=' + client_secret +
-    '&code='+ code +
-    '&grant_type=authorization_code' +
-    '&redirect_uri=' + redirect_uri)
-      .then(function (body) {
-        apiResponse = JSON.parse(body);
-        acccessToken = apiResponse.access_token;
+  rp('https://www.leaguevine.com/oauth2/token/' + '?client_id=' + client_id + '&client_secret=' + client_secret + '&code='+ code + '&grant_type=authorization_code' + '&redirect_uri=' + redirect_uri)
+    .then(function (body) {
+      apiResponse = JSON.parse(body);
+      acccessToken = apiResponse.access_token;
 
-        res.redirect('/teams');
+      res.redirect('/teams');
+    })
+    .catch(function (err) {
+      console.log('CALLBACK error');
     });
 }
 
 function getTeams (req, res) {
   rp('http://api.playwithlv.com/v1/tournament_teams/?tournament_ids=%5B20059%5D&access_token=' + acccessToken)
-      .then(function (body) {
-        var data = JSON.parse(body);
+    .then(function (body) {
+      var data = JSON.parse(body);
 
-        res.render('teams', {
-          teams: data.objects
-        });
+      res.render('teams', {
+        teams: data.objects
+      });
+    })
+    .catch(function (err) {
+      console.log('error getting TEAMS');
+    });
+}
+
+function getTeamDetail (req, res) {
+  //  https://api.leaguevine.com/v1/teams/29639/?access_token=f35d78be4c
+  console.log(req.params.id);
+
+  rp('http://api.playwithlv.com/v1/teams/'+ req.params.id + '/?access_token=' + acccessToken)
+    .then(function (body) {
+      var data = JSON.parse(body);
+      console.log(data);
+
+      res.render('teams-detail', {
+        teamName: data.name
+      });
+    })
+    .catch(function (err) {
+      console.log('error getting TEAM DETAIL');
     });
 }
 
 function getGames() {
-  console.log('games');
+  rp('http://api.playwithlv.com/v1/games/tournament_teams/?tournament_ids=%5B20059%5D&access_token=' + acccessToken)
+    .then(function (body) {
+      var data = JSON.parse(body);
+
+      res.render('games', {
+        games: data.objects
+      });
+    })
+    .catch(function (err) {
+      console.log('error getting GAMES');
+    });
 }
 
-// SOCKET THINGIESS HERE
+//  SOCKET THINGIESS HERE
 function onConnect (socket) {
-
+  //  socket stuff
 }
 
 var port = process.env.PORT || 8888;
 
 http.listen(port, function (){
-    console.log('server is running: on: ' + port);
+    console.log('server is running on: ' + port);
 });
