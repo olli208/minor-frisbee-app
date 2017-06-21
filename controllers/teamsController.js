938a3b9173c55405772c353271b7afccc1980a09
Oliver <oliveroenang@gmail.com>
2017-06-21 12:03:40 +0200

    get games on team page

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -5,9 +5,22 @@ var Team = mongoose.model('Team');
 exports.getTeamDetail = function (req, res) {
   rp(`http://api.playwithlv.com/v1/teams/${req.params.id}/?access_token=${req.session.accessToken}`)
     .then(function (body) {
-      var data = JSON.parse(body);
+      var teamData = JSON.parse(body);
 
-      res.render('teams-detail', {data});
+      return rp(`http://api.playwithlv.com/v1/games/?%5B${teamData.id}%5D&access_token=${req.session.accessToken}`)
+        .then(function (body) {
+          var teamGames = JSON.parse(body);
+
+          res.render('teams-detail', {
+            data: teamData,
+            games: teamGames.objects
+          });
+
+        })
+        .catch(function (err) {
+          console.log(`error getting TEAM DETAIL GAMES -> ${err}`)
+        })
+
     })
     .catch(function (err) {
       console.log('error getting TEAM DETAIL');
