9323c98a357b9f4b1c28fb65453bbfa253a1f413
Oliver <oliveroenang@gmail.com>
2017-06-21 13:07:07 +0200

    team-detail -> get games played from DB

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -1,28 +1,44 @@
 var rp = require('request-promise');
 var mongoose = require('mongoose');
-var Team = mongoose.model('Team');
+var mongoose = require('mongoose');
+var Game = mongoose.model('Game');
 
 exports.getTeamDetail = function (req, res) {
   rp(`http://api.playwithlv.com/v1/teams/${req.params.id}/?access_token=${req.session.accessToken}`)
     .then(function (body) {
       var teamData = JSON.parse(body);
 
-      return rp(`http://api.playwithlv.com/v1/games/?%5B${teamData.id}%5D&access_token=${req.session.accessToken}`)
-        .then(function (body) {
-          var teamGames = JSON.parse(body);
+      var games = Game.find({}).where({ "team_1.teamID": String(teamData.id) });
 
+      games
+        .then(function(games){
+          console.log(`games from DB -> ${games}`);
           res.render('teams-detail', {
-            data: teamData,
-            games: teamGames.objects
-          });
-
+                    data: teamData,
+                    games: games
+                  });
         })
-        .catch(function (err) {
-          console.log(`error getting TEAM DETAIL GAMES -> ${err}`)
+        .catch(function(err) {
+          console.log(`Could not find GAMES from DATABASE -> ${err}`)
         })
 
-    })
-    .catch(function (err) {
-      console.log('error getting TEAM DETAIL');
-    });
+    //   return rp(`http://api.playwithlv.com/v1/games/?%5B${teamData.id}%5D&access_token=${req.session.accessToken}`)
+    //     .then(function (body) {
+    //       var teamGames = JSON.parse(body);
+    //
+    //       res.render('teams-detail', {
+    //         data: teamData,
+    //         games: teamGames.objects
+    //       });
+    //
+    //     })
+    //     .catch(function (err) {
+    //       console.log(`error getting TEAM DETAIL GAMES -> ${err}`)
+    //     })
+    //
+    // })
+    // .catch(function (err) {
+    //   console.log('error getting TEAM DETAIL');
+    // });
+  })
 }
